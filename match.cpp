0c1d0755a9583f6181aee1e629cccc5f071fc151
brown0394 <73463402+brown0394@users.noreply.github.com>
2022-04-26 14:40:28 +0900

    minor_update
    space added at match.cpp

--- match.cpp
+++ match.cpp
@@ -6,7 +6,7 @@ int main ()
 	StringListProcess List(2);// declare process with 2 lists
 	cout << "List.Match2Lists (list1.txt,list2.txt,match.txt)" << endl;
 	List.Match2Lists ("list1.txt","list2.txt","match.txt");
-	cout << endl<<"List.Merge2Lists (list1.txt,list2.txt,match.txt)" << endl;
+	cout << endl << "List.Merge2Lists (list1.txt,list2.txt,match.txt)" << endl;
 	List.Merge2Lists ("list1.txt","list2.txt","merge.txt");
 	system("pause");
 	return 1;
